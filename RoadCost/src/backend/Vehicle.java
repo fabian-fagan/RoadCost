f5485538b70e9c5a99b650ceda04ab58524b4e56
Fabian <fabian.fagan99@gmail.com>
2021-05-26 15:16:28 +1200

    Update Vehicle.java

--- RoadCost/src/backend/Vehicle.java
+++ RoadCost/src/backend/Vehicle.java
@@ -1,5 +1,15 @@
 package backend;
 
 public class Vehicle {
+	private String type;
+	private int age; 
+	private boolean isDiesel;
+	
+	public Vehicle(String type, int age, boolean isDiesel) {
+		this.type = type;
+		this.age = age;
+		this.isDiesel = isDiesel; 
+				
+	}
 
 }
